f279843aa2cebc27066c347faea48ce72eaf8347
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-28 13:36:39 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ radio.onReceivedValue(function (name, value) {
 })
 let paddleB: game.LedSprite = null
 let paddleA: game.LedSprite = null
+radio.setGroup(69)
 paddleA = game.createSprite(2, 4)
 paddleB = game.createSprite(3, 4)
 let ball = game.createSprite(randint(0, 4), 0)
